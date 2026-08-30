24c60a938bc7cf21127c55f81c9a941fc66715fb
Royam1311 <royam1311@gmail.com>
2026-08-30 16:56:18 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './benefy-luxury.css';
+import './benefy-blue-theme.css';
 import './benefy-typography.css';
 import { enableCompactHeader } from './compactHeader';
 
