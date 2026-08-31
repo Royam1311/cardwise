ee2a022f94dc4be6a29af770bd303ff1a4cfceb2
Royam1311 <benefymail@gmail.com>
2026-08-31 15:36:38 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './benefy-typography.css';
 import './benefy-header-v5.css';
 import './benefy-search-dark.css';
 import './benefy-header-tokens.css';
-import './benefy-auth-showcase-v5.css';
+import './benefy-auth-showcase-v6.css';
 import { enableCompactHeader } from './compactHeader';
 import { enableProfilePopover } from './profileEnhancer';
 import { enableBenefyTokens } from './benefyTokens';
