b90e420680bb9df3c63504f6d0cedf33b3bbf99c
Royam1311 <benefymail@gmail.com>
2026-09-01 16:53:54 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ import './benefy-blue-theme.css';
 import './benefy-typography.css';
 import './benefy-header-v5.css';
 import './benefy-search-dark.css';
-import './benefy-header-tokens.css';
 import './benefy-auth-showcase-v9.css';
 import './benefy-nav-features.css';
 import { enableCompactHeader } from './compactHeader';
